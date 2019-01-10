2858049e8b058f0202ad510ca3a4d4ba52160c45
Kevin Broderick <kevin@discoverymap.com>
2019-01-10 15:14:38 -0500

    Update README for 0.8.0 gem instructions; update package.json to 0.8.0; update dist contents based on current package & Gruntfile

--- dist/jquery.dsmorse-gridster.js
+++ dist/jquery.dsmorse-gridster.js
@@ -1,6 +1,6 @@
-/*! gridster.js - v0.7.0 - 2017-03-27
+/*! gridster.js - v0.8.0 - 2019-01-10
 * https://dsmorse.github.io/gridster.js/
-* Copyright (c) 2017 ducksboard; Licensed MIT */
+* Copyright (c) 2019 ducksboard; Licensed MIT */
 
 ;(function(root, factory) {
 	'use strict';
@@ -2275,7 +2275,7 @@
 			container_width: this.container_width,
 			move_element: false,
 			resize: true,
-			limit: { width: this.options.max_cols !== Infinity || this.limit.width, height: this.options.max_rows !== Infinity || this.limit.height },
+			limit: { width: this.options.max_cols !== Infinity || this.options.limit.width, height: this.options.max_rows !== Infinity || this.options.limit.height },
 			scroll_container: this.options.scroll_container,
 			start: $.proxy(this.on_start_resize, this),
 			stop: $.proxy(function (event, ui) {
@@ -3265,9 +3265,7 @@
 	 */
 	fn.is_empty = function (col, row) {
 		if (typeof this.gridmap[col] !== 'undefined') {
-			if (typeof this.gridmap[col][row] !== 'undefined' &&
-					this.gridmap[col][row] === false
-			) {
+			if (!this.gridmap[col][row]) {
 				return true;
 			}
 			return false;
@@ -5031,7 +5029,7 @@
 	};
 
 	fn.get_num_widgets = function () {
-		return this.$widgets.size();
+		return this.$widgets.length;
 	};
 
 	/**
@@ -5178,157 +5176,5 @@
 	};
 
 	return Gridster;
-
-}));
-
-;(function(root, factory) {
-	'use strict';
-	if(typeof exports === 'object') {
-        module.exports = factory(require('jquery'), require('./jquery.gridster.js'));
-    }
-    else if (typeof define === 'function' && define.amd) {
-        define(['jquery', 'gridster'], factory);
-    } else {
-        root.Gridster = factory(root.$ || root.jQuery, root.Gridster);
-    }
-
-}(this, function($, Gridster) {
-	'use strict';
-    var fn = Gridster.prototype;
-
-    fn.widgets_in_col = function(col) {
-        if (!this.gridmap[col]) {
-            return false;
-        }
-
-        for (var i = this.gridmap[col].length - 1; i >= 0; i--) {
-            if (this.is_widget(col, i) !== false) {
-                return true;
-            }
-        }
-
-        return false;
-    };
-
-    fn.widgets_in_row = function(row) {
-        for (var i = this.gridmap.length; i >= 1; i--) {
-            if (this.is_widget(i, row) !== false) {
-                return true;
-            }
-        }
-
-        return false;
-    };
-
-    fn.get_bottom_most_occupied_cell = function() {
-        var row = 0;
-        var col = 0;
-        this.for_each_cell(function($el, c, r) {
-            if ($el && r > row) {
-                row = r;
-                col = c;
-            }
-        });
-
-        return {col: col, row: row};
-    };
-
-
-    fn.get_right_most_occupied_cell = function() {
-        var row = 0;
-        var col = 0;
-        this.for_each_cell(function($el, c, r) {
-            if ($el) {
-                row = r;
-                col = c;
-                return false;
-            }
-        });
-
-        return {col: col, row: row};
-    };
-
-
-    fn.for_each_cell = function(callback, gridmap) {
-        gridmap || (gridmap = this.gridmap);
-        var cols = gridmap.length;
-        var rows = gridmap[1].length;
-
-        cols_iter:
-        for (var c = cols - 1; c >= 1; c--) {
-            for (var r = rows - 1; r >= 1; r--) {
-                var $el = gridmap[c] && gridmap[c][r];
-                if (callback) {
-                    if (callback.call(this, $el, c, r) === false) {
-                        break cols_iter;
-                    } else { continue; }
-                }
-            }
-        }
-    };
-
-
-    fn.next_position_in_range = function(size_x, size_y) {
-        size_x || (size_x = 1);
-        size_y || (size_y = 1);
-        var ga = this.gridmap;
-        var cols_l = ga.length;
-        var valid_pos = [];
-        var rows_l;
-
-        for (var c = 1; c < cols_l; c++) {
-            rows_l = this.options.max_rows || ga[c].length;
-            for (var r = 1; r <= rows_l; r++) {
-                var can_move_to = this.can_move_to({
-                    size_x: size_x,
-                    size_y: size_y
-                }, c, r);
-
-                if (can_move_to) {
-                    valid_pos.push({
-                        col: c,
-                        row: r,
-                        size_y: size_y,
-                        size_x: size_x
-                    });
-                }
-            }
-        }
-
-        if (valid_pos.length >= 1) {
-            return Gridster.sort_by_col_asc(valid_pos)[0];
-        }
-
-        return false;
-    };
-
-
-    fn.closest_to_right = function(col, row) {
-        if (!this.gridmap[col]) { return false; }
-        var cols_l = this.gridmap.length - 1;
-
-        for (var c = col; c <= cols_l; c++) {
-            if (this.gridmap[c][row]) {
-                return { col: c, row: row };
-            }
-        }
-
-        return false;
-    };
-
-
-    fn.closest_to_left = function(col, row) {
-        if (!this.gridmap[col]) { return false; }
-
-        for (var c = col; c >= 1; c--) {
-            if (this.gridmap[c][row]) {
-                return { col: c, row: row };
-            }
-        }
-
-        return false;
-    };
-
-    return Gridster;
 
 }));
